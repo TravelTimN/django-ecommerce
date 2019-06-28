f02f6c554f7a4bdf1eff7ab970c2fafe3f9b4dda
Tim Nelson <traveltimn@gmail.com>
2019-06-28 22:02:26 +0100

    Setting up S3 Buckets with AWS

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'checkout',
     'home',
     'products',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -106,14 +107,28 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_S3_OBJECT_PARAMETERS = {
+    "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+    "CacheControl": "max-age=94608000",
+}
+AWS_STORAGE_BUCKET_NAME = "django-ecommerce-2bntim"
+AWS_S3_REGION_NAME = "eu-west-1"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY= os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = "static"
+STATICFILES_STORAGE = "custom_storages.StaticStorage"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "/media/"
+MEDIAFILES_LOCATION = "media"
+DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.getenv("STRIPE_SECRET")
