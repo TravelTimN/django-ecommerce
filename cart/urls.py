7df8791bee19360508d50208a98dc84b180246dd
Tim Nelson <traveltimn@gmail.com>
2020-01-17 21:06:41 +0000

    Update Django url to path

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import view_cart, add_to_cart, adjust_cart
 
 
 urlpatterns = [
-    url(r'^$', view_cart, name="view_cart"),
-    url(r'^add/(?P<id>\d+)', add_to_cart, name="add_to_cart"),
-    url(r'^adjust/(?P<id>\d+)', adjust_cart, name="adjust_cart"),
+    path("", view_cart, name="view_cart"),
+    path("add/<int:id>", add_to_cart, name="add_to_cart"),
+    path("adjust/<int:id>", adjust_cart, name="adjust_cart"),
 ]
